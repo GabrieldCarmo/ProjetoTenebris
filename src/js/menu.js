8e9031e96e5d34bb5a36d0546b3ef4c10e2c0a26
Gabriel do Carmo <gabrieldcarmo1@gmail.com>
2026-02-18 17:19:12 -0300

    27 - Criando js do menu e ajustando header de todas as páginas

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,9 +1,37 @@
-// // Seleciona o botão e o menu
-// const menuToggle = document.getElementById('menu-toggle');
-// const nav = document.getElementById('nav');
+const menuBtn = document.getElementById("menu-hamburguer");
 
-// // Adiciona o evento de clique
-// menuToggle.addEventListener('click', () => {
-//   nav.classList.toggle('active');
-//   menuToggle.classList.toggle('open');
-// });
+const menuBox = document.createElement("div");
+menuBox.classList.add("menu-box");
+
+menuBox.innerHTML = `
+    <ul>
+        <li><a href="/index.html">inicio</a></li>
+        <li><a href="/src/pages/alien.html">Alien</a></li>
+        <li><a href="/src/pages/chamado.html">Chamado</a></li>
+        <li><a href="/src/pages/panico.html">Pânico</a></li>
+        <li><a href="/src/pages/sobrenos.html">Sobre Nós</a></li>
+    </ul>
+`;
+
+document.body.appendChild(menuBox);
+
+menuBtn.addEventListener("click", (e) => {
+    e.stopPropagation(); // impede conflito com o click global
+    menuBox.classList.toggle("ativo");
+});
+
+document.addEventListener("click", (e) => {
+    if (!menuBox.contains(e.target) && e.target !== menuBtn) {
+        menuBox.classList.remove("ativo");
+    }
+});
+
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 20) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
